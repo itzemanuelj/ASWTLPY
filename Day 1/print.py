5261c4534e8f6e0cc6d8ba70fee826cf79e58e43
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-24 20:45:22 -0500

    mvp commit day 1 and day 2

---
+++ Day 1/print.py
@@ -0,0 +1,26 @@
+# What keyword tells Python to display something on the screen?
+print("hello world")
+
+# Complete the line of code to tell Python to display the name of the oval object laid by a hen. (1 word, lower-case)
+print("egg")
+
+# Tell Python to display the letter "e
+print("e")
+
+# Tell Python to display the name of our planet. (1 word, lower-case)
+print("earth")
+
+# Tell Python to display the name of the star that makes life possible on earth. (1 word, lower-case)
+print("sun")
+
+# Tell Python to display the first three letters of the alphabet in upper-case.
+print("abc")
+
+# Tell Python to display your first name.
+print("Justin")
+
+# Tell Python to display the three-letter word for a canine. (1 word, lower-case)
+print("dog")
+
+# write a statement telling Python to display the string "Hello, World!"
+print("Hello, World!")
---
+++ Day 2/Variables for Strings.py
@@ -0,0 +1,34 @@
+# Because of the quotation marks, Python identifies the greeting below as a _______(1 word, lowercase)
+"Hello, World!"
+# string
+
+# Because there are no quotation marks, Python identifies the set of characters below as a _______.
+country_of_origin = "USA"
+# variable
+
+# Assign the string "Boo!" to the variable scare.
+scare = "Boo!"
+
+# Assign the string "jay" to the variable bird.
+bird = "jay"
+
+# Assign your first name to a variable. You choose the variable name.
+fname = "Justin"
+
+# Assign a string to a variable. You choose the variable name and the string.
+f_name = "Justin"
+
+# In the statement below, I assign a string to a variable. Using the variable, write the statement that displays the string on the screen.
+my_name = "Mark"
+print(my_name)
+
+# Assign a string to a variable. You choose the variable name and the string. Using the variable, write a statement that displays the string on the screen.
+team_name = "Detroit Pistons"
+print(team_name)
+
+# Revise this variable name as the Python governing body recommends.
+# countryoforigin
+country_of_origin
+
+city_Name = "Detroit"
+print(city_Name)
