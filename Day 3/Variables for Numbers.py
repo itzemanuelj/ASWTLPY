6dea6dc117c4227435a7e1c8e96469b9902cbeca
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-25 13:41:10 -0500

    mvp commit for day 3

---
+++ Day 3/Variables for Numbers.py
@@ -0,0 +1,41 @@
+# There are two correct answers for this one. Answer with either one. What is this?
+100
+# number or integer
+
+# Type the illegal character in this variable name.
+# 2nd_runner_up
+runnner_up = True
+
+# Assign the number 5 to the variable players
+players = 5
+# Assign the negative number -6 to the variable loss
+loss = -6
+
+
+# What is the value of total?
+number = 10
+total = number + 2
+print(total)
+
+# Assign a number(your choice) to the variable total.
+total_cost = 250
+
+# What is this(one word, lowercase)?
+"-8.087"
+# string
+
+# Add two variables and assign the result to a third variable. Make up all the variable names.
+
+cost = 1
+time = 10
+total = cost + time
+
+# Add 5 to the variable running_total and assign the result to the same variable
+
+running_total = 10
+running_total = running_total + 5
+
+# assign the sum of two numbers to a variable. Make up the variable name and the numbers. On line 2, tell Python to display the sum using the variable.
+
+time_total = cost + time
+print(time_total)
