f04db3b030687832b103ff3856ddc82c4e96bdc5
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-13 02:39:02 -0500

    Create Math expressions: Familiar operators.py

---
+++ Day 4/Math expressions: Familiar operators.py
@@ -0,0 +1,42 @@
+# Rewrite this so total equals 10.
+total = 4 + 6
+
+first_number = 5
+second_number = 6
+third_number = first_number * second_number
+
+print (third_number)
+# 30
+
+# Divide 12 by the decimal .5 and assign the result to the variable
+
+cost = 12 / .5
+print (cost)
+
+# Divide the variable total by 3 and assign the result to the variable each_share.
+
+each_share = total / 3
+print(each_share)
+
+# Subtract one number from another and assign the result to a variable. Make up the numbers and the variable name.
+
+time = 100 - 10
+print(time)
+
+time = cost - 10
+print(time)
+
+times_three = 3 * 3 *3
+print(times_three)
+
+#in one line of code, display the result of multiplying two variables. Make up the variable names.
+
+cost = time * total
+print(cost)
+
+print(time * cost)
+
+#In a single line of code, divide one number by another and display the result.
+
+print(100 / 5)
+
