536dc2909bc29be52f4ccc85b067b4034192e63a
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-13 02:47:03 -0500

    Create Variable Names Legal and Illegall.py

---
+++ Day 5/Variable Names Legal and Illegall.py
@@ -0,0 +1,44 @@
+# Type the illegal character in this variable name:
+nd_address = 2
+
+# Type the illegal character in this variable name:
+# fourth-Player
+# -
+
+# Rewrite this variable name using the recommended style: customerlastname
+
+custom_last_name = "Justin"
+
+# Rewrite this variable name using the recommended style:
+totalaftertax = 100
+
+total_after_tax = 100
+
+# Make up a variable name that includes a number
+
+customer_01 = "Andrei Neorgie"
+
+# Assign a string to a variable. Make up the string and the variable name.
+
+city_team = "Detroit Pistons"
+
+# Assign the sum of two numbers to a variable. Make up the numbers and the variable name.
+
+total_cost = 2 + 10
+
+# Divide one variable by another and assign the result to a third variable. Make up the variable names.
+
+
+total = 100
+cost = total / total_cost
+
+
+# 1stvariable = 1
+# another-variable = 2
+# onemorevariable = 3
+# print(another-variable)
+
+str_variable = 1;
+another_variable = 10
+one_more_variable = 100
+print(another_variable)
