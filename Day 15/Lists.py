834cd8b0efcb855a71d13bacf3b1e50bdfbab648
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-16 12:37:01 -0500

    mvp commit for day 15 python

---
+++ Day 15/Lists.py
@@ -0,0 +1,52 @@
+# What is players? (one word)
+
+players = ["Jane", "Jen", "Jill"]
+
+# list
+
+# Type the two characters that are missing from this list definition.
+
+scores = [78, 47, 3, 52, 16]
+
+# The last element in the list is 0. Complete the list definition.
+
+qtys = [144, 12, 16, 0]
+
+# Define a list, noncolors, that has a single element, "black".
+
+noncolors = ["black"]
+
+#What is the index number of a list's first element
+0
+
+# A list contains 20 elements. What is the index number of the last element?
+
+19
+
+# Assign the third element in the list to a variable. Make up the variable name.
+
+cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"]
+
+third_on_list = cities[2]
+
+# Code a list whose two elements are strings. Make up the name of the list and the strings.
+
+contries = ["USA", "Canada"]
+
+#In a single line of code and using the list to specify the element, display "Black".
+
+seas = ["North", "Red", "Dead", "Black", "Baltic", "Bali"]
+print(seas[3])
+
+#assign the last element of the list to the variable.
+
+weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+fifth_weekday = weekdays[4]
+
+print(fifth_weekday)
+
+# Define a list that contains three elements.
+# Display the list.
+
+teams = ["detroit pistons" ,"motorcity cruise" , "detroit lions"]
+print(teams[2])
