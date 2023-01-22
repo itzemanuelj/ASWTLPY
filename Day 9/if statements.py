357b641c83000549f19382c1a514e2419767d1e2
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-22 00:47:25 -0500

    if statements in python

---
+++ Day 9/if statements.py
@@ -0,0 +1,49 @@
+# if statements
+
+# Rewrite this line so it's valid:
+species = ["cat"]
+
+if species == "cat":
+    print(species)
+
+# Write the first line of an if statement that tests whether species is "Dog"    
+if species == "Dog":
+    print(species)
+
+# Code the first line of an if statement that tests whether x has the same value as y
+x = "a"
+y = "b"
+
+if x == y:
+    print(x)
+
+#Below, I've coded the first line of an if statement. Code the second line. The second line tells Python to display the string "OK". Don't forget to indent the second line.
+current_address = "4885"
+previous_address = "48224"
+
+if current_address == previous_address:    
+    print("OK")
+
+# If total has the value of 100, tax has the value of 2. Code both lines. Don't forget to indent the second line.
+tax = 2
+
+if tax == 100:
+    tax = 2
+    print()
+
+# If first_name is "Sherlock" last_name is "Holmes" and pal is "Watson". Code all three lines.
+
+if first_name == "Sherlock":
+    last_name == "Holmes"
+    pal == "Watson"
+    print(first_name + " " + last_name)
+
+# If you_won is "yes" tell Python to display "congrats". Code both lines.
+
+if you_won == "yes":
+    print("congrats")   
+
+
+    species = "human"
+if species == "human":
+  print("correct")
