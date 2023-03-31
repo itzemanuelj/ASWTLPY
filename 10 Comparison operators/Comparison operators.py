f05ddc2973b1958bebd3b07c55798116b577dcd9
Emanuel Joness <itzemanuelj@gmail.com>
2023-03-31 14:33:29 -0400

    mvp commit for comparison operators

---
+++ 10 Comparison operators/Comparison operators.py
@@ -0,0 +1,75 @@
+# Comparison operators
+
+# What character means "greater than"?
+
+# What character means "greater than"?
+
+a = 1
+if a > 10:
+    print("a")
+
+# What character means "less than"?
+
+if a < 1:
+    print("a")
+
+# Code the first line of an if statement testing whether the variable total is less than 100.
+
+total = 100
+if total < 100:
+    print(total)
+
+# Code the first line of an if statement testing whether the variable tax is equal to the variable minimum.
+
+tax = 1
+minimum = 10
+
+if tax == minimum:
+    print("same number")
+
+# How do you write less than or equal to?
+
+if a <= 10:
+    print("")
+
+# How do you write greater than or equal to?
+
+if a >= 10:
+    print("")
+
+# Code the first line of an if statement that tests whether the sum of a and b is greater than or equal to c minus 7.
+
+b = 0
+c = 1
+
+if (a + b >= c - 7):
+    print("")
+
+# Write the first line of an if statement that tests whether one variable is greater than or equal to another variable. Make up the variable names.
+
+time = 10
+travel = 10
+
+if time >= travel:
+    print("")
+
+# Write an if statement that tests whether a variable is smaller than 99. If the test passes, assign 99 to the variable. Make up the variable name. Remember to indent the second line two spaces.
+
+grade = 80
+
+if grade <= 99:
+    print(grade)
+
+# Write an if statement that tests whether a first variable is greater than a second variable. If the test passes, make the second variable equal to the first variable. Make up the variable names.
+
+if grade > total:
+    print("")
+
+# If 1 plus 1 doesn't equal 3, display ok
+if 1 != 3:
+    print("ok")
+
+# If 2 is less than or equal to 2, display ok
+
+if 2 <= 2:
+    print("ok")
