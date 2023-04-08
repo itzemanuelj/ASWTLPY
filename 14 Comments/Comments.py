a1a7033ef757b79ace2853fa858861e600a080be
Emanuel Joness <itzemanuelj@gmail.com>
2023-04-08 18:23:09 -0400

    mvp comment for comments

---
+++ 14 Comments/Comments.py
@@ -0,0 +1,74 @@
+## Comments
+
+## Type the character that begins a one-line comment.
+## hello world
+
+## Type the characters that begin a block comment.
+
+'''
+hello
+world !
+'''
+
+## Code a comment that says: This is a comment
+
+## this is a comment
+
+## code the characters that begin a block comment. On line 2 type: This is a comment. On line 3 code the characters that end a block comment.
+
+'''
+this is a comment
+!
+'''
+
+## Code a 1-line comment. Make up the comment. Remember to type a space after the first character.
+
+
+## this is a random comment
+
+## Code two 1-line comments. Make up the comments.
+
+'''
+this is a mult line comment
+'''
+
+## Code a block comment that has only one line of text.
+
+## this is a single line quote
+
+## Code a 1-line comment. Then write a line of live code that assigns a number to a variable. Make up the comment, the variable name, and the number.
+
+## let a = 1
+
+## Code a block comment that has two lines of text. Make up the text.
+
+'''
+two
+line comment
+'''
+
+## Rewrite this, commenting out the illegal lines.
+a = 1
+## b = two sisters
+c = 3
+d = 4 + "dog"
+
+## Using #, comment out the illegal lines.
+
+next_number = 1
+
+a = 1
+if a == 1:
+## "cat" = species
+    next_number == 2
+print("ok")
+
+## Rewrite the first four lines as a comment block.
+
+'''
+This is a comment.
+This is another comment.
+This is a third comment.
+This is a fourth comment.
+print("ok")
+'''
