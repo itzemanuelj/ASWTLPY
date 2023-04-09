e7e776939b614766c28fb45bfadc15b48e7c70a9
Emanuel Joness <itzemanuelj@gmail.com>
2023-04-09 13:31:41 -0400

    mvp commit for Lists: Adding and changing elements

---
+++ 16 Lists: Adding and changing elements/Lists: Adding and changing elements.py
@@ -0,0 +1,90 @@
+## Lists: Adding and changing elements
+
+## appemd 
+## inserst
+
+## What is the keyword for adding a new element to the end of a list?
+
+list_one = []
+
+list_one.append = "a"
+
+## Using the append keyword, add the number 1 to the end of the list nums.
+nums = []
+nums.append(1)
+
+## Using the keyword append, add the string "cat" to the end of the list pets.
+ 
+pets = []
+
+pets.append("cat")
+
+## Using the keyword append, add the string "sun" to the end of the list stars.
+
+stars = []
+
+stars.append("sun")
+
+## Code the alternative to the following statement, using the plus sign.
+x = 1
+
+x.append(5)
+x = x + [5]
+
+## Using the append keyword, append a string to a list. Make up the string and list name.
+
+names = []
+names.append("Justin")
+
+## Using the plus sign instead of append, add two strings to the end of a list. Make up the list name and strings.
+
+
+names = names + ["Justin" + "Jones"]
+
+## Insert the number 5 at the beginning of a list. Make up the list name.
+
+numbers = []
+
+numbers.insert(0, 5)
+
+## n the following list, insert a string after "orange". Make up the string.
+
+fruits = ["apple", "orange", "tangerine", "banana"]
+
+fruits.insert(2,"grape")
+
+## Using the plus sign rather than append, add two numbers to the end of a list and assign the result to a second list. Make up the list names and the numbers.
+
+total = []
+second_total = []
+
+total = second_total + [7, 11]
+
+## Fill in the blank to assign the fifth element of the list to the variable.
+
+weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+fifth_weekday = weekdays.append[4]
+print(fifth_weekday)
+
+# Create a list of three numbers. Then display the list.
+
+prices = [1, 2 ,3]
+print(prices)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
