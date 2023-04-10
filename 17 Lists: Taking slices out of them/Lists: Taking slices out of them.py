0d064d630525efae20d014ece4d037e59b8d0265
Emanuel Joness <itzemanuelj@gmail.com>
2023-04-10 15:29:20 -0400

    mvp commit for lists slices
    ## Lists: Taking slices out of them

---
+++ 17 Lists: Taking slices out of them/Lists: Taking slices out of them.py
@@ -0,0 +1,63 @@
+## Lists: Taking slices out of them
+
+## In the following code, smaller_list_of_cities is assigned __ elements of the cities list. (Enter a numeral.)
+
+cities = []
+
+smaller_list_of_cities = cities[2:5]
+
+3
+
+## Fill in the blank. The first element in the slice is the second element in the list. The last element in the slice is the third element in the list.
+smaller_list_of_cities = cities[1:3]
+
+## Copy the second through eighth elements from the list y and assign the slice to the list x.
+
+y = []
+
+x = y[1:8]
+
+## Reduce the list x to a slice of itself—the sixth through 8th elements.
+
+x = x[5:8]
+
+## Copy some elements from a list and assign the slice to another list. Make up the list names and the index numbers.
+
+teams = ["detroit", "grosse pointe" , "eastpointe"]
+
+sports_team = teams[1:3]
+
+## What is the index number of the last element in the slice?
+x = y[7:9]
+
+## 8
+
+## Fill in the blank to slice the first through ninth element with minimal coding.
+x = y[:9]
+
+## Using minimal coding, copy the fifth through last element from y and assign the slice to x.
+
+y = x [4:]
+
+## Copy the numbers 2 through 4 out of y and assign the slice to x.
+y = [1, 2, 3, 4, 5]
+
+x = y[1:4]
+
+## Using minimal coding, reduce the list x to its first five elements.
+
+y = x[:5]
+
+## Copy the middle two numbers out of list y and assign the slice to list x. Display list x.
+y = [10, 11, 12, 13, 14, 15, 17, 18]
+
+y = x[3:5]
+print(x)
+
+
+## Using minimal coding, reduce the list x to "elm" and "chestnut." Then display the list.
+x = ["elm", "chestnut", "oak", "palm", "fir"]
+x = x[2]
+print(x)
+
+
