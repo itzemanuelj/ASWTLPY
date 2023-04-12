e0260336593fbcd78a1348bc2041104ce4b2c759
Emanuel Joness <itzemanuelj@gmail.com>
2023-04-12 12:05:09 -0400

    mvp commit for Lists: Deleting and removing elements
    ## Lists: Deleting and removing elements in python

--- 18 Lists: Deleting and removing elements/Lists: Deleting and removing elements.py
+++ 18 Lists: Deleting and removing elements/Lists: Deleting and removing elements.py
@@ -4,3 +4,80 @@ a = []
 
 del[]
 
+## What is the keyword for eliminating a list element by specifying its value?
+
+tasks = ['take out trash', 'run errands']
+
+tasks.remove('take out trash')
+
+## Eliminate the element with an index of 2 from a list whose name is products.
+
+products = ['products']
+
+del products[2]
+
+## Eliminate "lamp" from the products list.
+
+products.remove('lamp')
+
+## Eliminate "lamp" from the list using its index number.
+
+products = ["chair", "table", "lamp", "rug", "couch"]
+
+products.remove('lamp')
+
+
+del products[2]
+
+## Eliminate "chair" from the list using its value.
+
+products = ["chair", "table", "lamp", "rug", "couch"]
+
+del products[0]
+
+products.remove('chair')
+
+## Eliminate an element from a list using its index number. Make up the list name and index number.
+
+teams = ['pistons', 'lions', 'tigers']
+
+del teams[2]
+
+## Using remove, eliminate a string element from a list using its value. Make up the list name and the value.
+
+teams.romove('tigers')
+
+
+## On one line add a string to the end of a list using the append keyword. On the next line eliminate it by specifying its value. Make up the string and the name of the list.
+
+teams.append('red wings')
+teams.remove('red wings')  
+
+## or
+
+teams.append('red wings')
+teams.remove('red wings')
+
+## Eliminate "rose" by specifying its index number.
+
+flowers = ["hyacinth", "tulip", "lotus", "rose", "geranium"]
+
+del flowers[3]
+
+## Eliminate "sink" from the list using its index number. Then display the list.
+
+products = ["lamp", "table", "chair", "couch", "sink"]
+
+products.remove('sink')
+
+print(products)
+
+## Eliminate "sink" from the list using its value. Then display the list.
+
+del products[4]
+print(products)
+
+
+
+
+
