9f0421d9e082cfa0f1187fdd5df3c2a274e53ed9
Emanuel Joness <itzemanuelj@gmail.com>
2023-04-14 11:20:30 -0400

    mvp commit for tuples in python

---
+++ 20 Tuples/Tuples.py
@@ -0,0 +1,69 @@
+## Tuples
+
+## It's like a list, but the elements are fixed. What is it called?
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)
+
+## Recode this list to make it a tuple.
+x = [10, 9]
+
+x = (10, 9)
+
+print(x)
+
+## Define a tuple with two elements, "hi" and "bye". The tuple's name is greetings.
+
+greeting = ("hi", "bye")
+
+print(greeting)
+
+##Define a tuple with two elements, 1 and 2. The tuple's name is nums.
+
+nums = (1, 2)
+print(nums)
+
+## Rewrite this so you can make changes to the elements.
+sizes = ("sm", "med", "lg", "xl")
+
+sizes = ["sm", "med", "lg", "xl"]
+
+print(sizes)
+
+numbers = (1, 2, 3 ,4)
+print(numbers)
+
+## Concatenate the second element of tuple x with the third element of tuple y. In the concatenation include a comma and space in the middle. Assign the concatenated string to a variable. Make up the variable name.
+
+x = (3, "Hello", "Howdy", "Greetings")
+y = ("earth", "sun", "World", "Pluto", 144)
+
+greeting = x[1] + ", " + y[2]
+
+## Code a tuple with three elements—your first, middle, and last names. Make up the tuple name.
+
+name = ("justin", "emanuel", "jones")
+print(name)
+
+## Code a tuple with three numbers. Make up the tuple name and the numbers.
+
+three_nums = (1, 2, 3)
+print(three_nums)
+
+##Code a tuple with three elements that are strings. Then, in a single line, append the last element to a list. Make up the names and strings.
+
+##fruits = ("apple", "orange", "bannana")
+##fruits.append(fruits[2])
+
+## create a tuple with at least two elements. Then display the second element.
+
+times = (1, 2, 3)
+print(times[1])
+
+## Insert an element of the tuple into the first position (index 0) in the list. Then display the list.
+
+list = [4, 5, 6]
+tuple = (1, 2, 3)
+
+list.insert(0, tuple[2])
+print(list)
+
