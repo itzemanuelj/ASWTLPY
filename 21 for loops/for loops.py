f1cd0a6be35e679f580a766d89dcade589541744
Emanuel Joness <itzemanuelj@gmail.com>
2023-04-15 16:04:33 -0400

    mvp commit for for loops

---
+++ 21 for loops/for loops.py
@@ -0,0 +1,81 @@
+## for loops
+
+## What is the name of the list that we're looping through here?
+
+cleanest_cities = []
+
+for a_clean_city in cleanest_cities:
+    print(cleanest_cities)
+    
+## cleanest_cities
+
+## What is the name of the variable that stores a list element each time through the loop?
+
+for a_clean_city in cleanest_cities:
+    print(cleanest_cities)
+
+## a_clean_city
+
+## Looping through the list cleanest_cities, complete the line.
+
+for a_clean_city in cleanest_cities:
+    print(a_clean_city)
+    
+## We're looping through the list y and assigning each element to the variable x. Code the first line.    
+
+x = []
+y = []
+
+for x in y:
+    print(x)
+    
+## Looping through y, we're testing if x has the same value as z. Write the second line. Don't forget to indent the line.
+
+z = []
+
+for x in z:
+    print(z)
+    
+## Write the first line of a for loop. Make up the variable name and the list name.
+
+nums = []
+
+for counter in nums:
+    print(counter)
+    
+## Write the first two lines of a for loop. The second line tests whether the list element is greater than or equal to 99. Make everything else up. Don't forget to indent the second line.  
+
+for counter in nums:
+    if counter >= 99:
+        print("")
+
+names = ["justin", "emanuel" , "jones"]
+
+for name_list in names:
+    print(name_list)
+    
+## Loop through a list. If the element has the same value as some variable, display "ok". Make everything up.
+
+documents = ["name", "date", "birth"]
+
+for doc_info in documents:
+    if doc_info == doc_info:
+        print(doc_info)
+
+## Loop through the tuple. If one of the elements has the same value as my_street, display "yes"
+
+my_street = "Main"
+streets = ("Elm", "Walnut", "Center", "Main", "Front")
+
+for main_street in streets:
+    if main_street == my_street:
+        print("yes")
+
+my_number = 0
+your_number = 1
+
+nums = [1,2,3,4,5]
+for counter in nums:
+  if counter == my_number:
+    print('no')
+
