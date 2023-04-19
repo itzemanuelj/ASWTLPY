bae1c6208676d16e9c07b076caa9fd8155401dda
Emanuel Joness <itzemanuelj@gmail.com>
2023-04-18 23:04:10 -0400

    mvp commit for Changing case in python
    Changing case in python

---
+++ 24 Changing case/Changing case.py
@@ -0,0 +1,60 @@
+## Changing case
+
+## Complete this code to convert to lowercase:
+big_letters = ["A"]
+small_letters = big_letters.lower()
+
+## Complete this code to convert to lowercase:
+small_letters = big_letters.lower()
+
+## Convert the string stored in the variable names to lowercase and assign the converted string to the same variable, names.
+
+names = ["detroit"]
+names = names.lower()
+
+## Convert the string stored in y to uppercase and assign the converted string to x
+
+y = ["detoit"]
+x = y.upper()
+
+## Convert "abc" to "ABC" and store the converted string in a variable. Make up the variable name.
+
+abc = ["abc"]
+upper_abc = abc.upper()
+
+## Convert "robin hood" to "Robin Hood" and store the converted string in a variable. Make up the variable name.
+robinhood_title = "robin hood".title()
+
+## Convert the string stored in the variable to lowercase and assign the converted string to the same variable.
+name = "aBRogasT"
+name = name.lower()
+
+## Convert the string stored in the variable to uppercase and assign the converted string to a second variable. Make up the second variable's name.
+name = "aBRogasT"
+upper_name = name.upper()
+
+## Convert the string stored in a variable to uppercase. Then convert it to initial capital letters. Keep assigning the converted strings to that same variable. Make up the variable name.
+
+fname = ["justin"]
+fname = fname.upper()
+fname = fname.title()
+
+## Run a for loop to convert all the elements of a list to lowercase. Make up the list name and the variable name.
+for name in fname:
+    name = name.lower()
+    
+## In a single line, capitalize the first letters and display the converted string.
+city = "bengaluru, india"
+city = city.title()  ## print(city.title())
+print(city)
+
+
+## Run a for loop that converts all the elements in the list to lowercase and displays each converted string.
+cities = ["cairo", "Turin", "manila", "Reykjavik", "riO", "JOHANNESBURG", "mELbourNE"]
+
+for new_cities in cities:
+    new_cities = new_cities.lower()
+    print(new_cities)
+
+
+
