f9355b3eff365ee4076e2370befad7581ea8ed72
Emanuel Joness <itzemanuelj@gmail.com>
2023-04-19 21:00:34 -0400

    mvp commit for Dictionaries: How to code one
    Dictionaries: How to code one

---
+++ 26 Dictionaries: How to code one/Dictionaries: How to code one.py
@@ -0,0 +1,38 @@
+## Dictionaries: How to code one
+
+## Type the two characters that are missing from this dictionary definition.
+dictionary_of_capitals = {"Thailand": "Bangkok", "Mexico": "Mexico City", "Canada": "Ottawa"}
+
+## Type the character that is missing from this dictionary definition. It's missing three times, but type it just once.
+dictionary_of_capitals = {"Thailand": "Bangkok", "Mexico": "Mexico City", "Canada": "Ottawa"}
+
+## Code a list named colors. The elements are "red" and "blue".
+colors = ["red", "blue"]
+
+## Code a dictionary named relatives. It has only one item. "brother" is paired with "Bob"
+realitives = {'brother': 'Bob'}
+
+## Code an empty dictionary. Make up the name of the dictionary.
+empty_dictionary = {}
+
+## A dictionary contains pairs. A key is paired with a value.
+
+## Define a dictionary with two items. The keys are "population" and "life_expectancy". The values are numbers. Make up the dictionary name and the values.
+
+research = {'population': '1000', 'life_expectancy': '2000'}
+
+## Code a dictionary with two key-value pairs. The keys are numbers. The values are strings. Make up the dictionary name, the keys, and the values.
+
+nums = {'one': 1, 'two': 2}
+
+## Code a dictionary with two key-value pairs. The keys are strings. The values are numbers. Make up the dictionary name, the keys, and the values.
+
+champs = {'detroit': 'michigan', 'cleveland': 'ohio'}
+
+## Define a 3-item dictionary. Both the keys and the values are strings. Display the dictionary.
+
+pets = {'cat': 'tina', 'dog': 'snuggles', 'bunny': 'kkatamina'}
+
+## Code a dictionary with just two items. The first item is a number key paired with a string. The second item is a string key paired with a number. Display the dictionary.
+
+random = {1: 'one', 2: 'two'}
