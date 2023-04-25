5f690a3dfe449600edc41796c725e5fff1fafab6
Emanuel Joness <itzemanuelj@gmail.com>
2023-04-24 22:58:04 -0400

    mvp commit for Dictionaries: The versatility of keys and values
    Dictionaries: The versatility of keys and values

---
+++ 28 Dictionaries: The versatility of keys and values/Dictionaries: The versatility of keys and values.py
@@ -0,0 +1,76 @@
+## Dictionaries: The versatility of keys and values
+
+## A key can be a string. It can also be a number
+
+## A value paired with a key can be a number. It can also be a string
+
+## The dictionary has just one item. The dictionary's name is products. The key is 101. The value is "wrench." Code the dictionary.
+
+products = {101: 'wrench'}
+
+## The dictionary has just one item. The dictionary's name is x. The key is "A12". The value is 9.99 Code the dictionary.
+
+x = {'A12': 9.99}
+
+
+## Code a dictionary with a single item. Both the key and the value are numbers. Make everything up.
+numbers = {101: 100}
+## Code a dictionary with a single item. Both the key and the value are strings. Make everything up.
+
+first_names = {'name': 'justin'}
+
+## Code a one-item dictionary. The key is a number. The value is a string. Make everything up.
+
+customer = {'10893892:': 'justin'}
+
+## Code a one-item dictionary. The key is a string. The value is a number. Make everything up.
+
+my_dict = {'key': 42}
+
+dicts = {
+    'name': 'justin',
+    'name': 'justin',
+    'name': 'justin',
+    'name': 'justin'
+}
+
+## Code a three-item dictionary, breaking the code into five lines. All keys are numbers. All values are strings. Make everything up.
+list_of_5 = {
+    1: 'one', 
+    2: 'two', 
+    3: 'three',
+    4: 'four',
+    5: 'five'
+}
+
+## Code a three-item dictionary, breaking it into five separate lines. Then display one of the values.
+
+pets = {'pet1': 'dog',
+        'pet2': 'cat',
+        'pet3': 'goffer'
+        }
+print(pets['pet1'])
+
+prices = {
+  "screw": .03,
+  "bolt": .075,
+  "washer": .015,
+}
+print(prices["bolt"])
+
+## Code a three-item dictionary on five lines. The keys are integers. The values are floats. Display the sum of the dictionary's three values.
+
+nums = {
+    1: 1.1,
+    2: 2.2,
+    3: 3.3    
+}
+
+print(nums[1]+ nums[2]+ nums[3])
+
+
+
+
+
+
+
